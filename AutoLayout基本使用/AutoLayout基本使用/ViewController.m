f23e1c2de417f1aafa0434fba372a1e5d2dec7f9
王浩 <bingoogolapple@gmail.com>
2015-04-05 22:25:46 +0800

    add AutoLayout基本使用

--- AutoLayout基本使用/AutoLayout基本使用/ViewController.m
+++ AutoLayout基本使用/AutoLayout基本使用/ViewController.m
@@ -8,6 +8,13 @@
 
 #import "ViewController.h"
 
+/*
+注意：如果使用autolayout来约束控件，那frame就失效了，官方也不建议我们再设置frame了
+如果利用autolayout约束一个控件，和我们以前使用frame约束一样，必须设置宽度/高度/x/y,如果缺少某一个约束就会报错，有可能会引发一些未知bug
+ 
+ 在使用autolayout时，最好给每个控件起一个名字
+ iOS8默认情况下，左右两边会留出一段距离
+*/
 @interface ViewController ()
 
 @end
@@ -16,12 +23,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
