7c0252c93048b6aadd18b37922d56be21738cb43
王浩 <bingoogolapple@gmail.com>
2015-04-19 11:59:00 +0800

    完成九宫格锁屏

--- 手势解锁/手势解锁/BGALockView.m
+++ 手势解锁/手势解锁/BGALockView.m
@@ -8,6 +8,14 @@
 
 #import "BGALockView.h"
 
+@interface BGALockView()
+
+@property (nonatomic, strong) NSMutableArray *selectedBtns;
+// 当前手指移动所在位置
+@property (nonatomic, assign) CGPoint moveP;
+
+@end
+
 @implementation BGALockView
 
 // 手动创建时会调用该方法
@@ -35,8 +43,11 @@
 -(void)addBtns {
     for (int i = 0; i < 9; i++) {
         UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-        [button setImage:[UIImage imageNamed:@"gesture_node_highlighted"] forState:UIControlStateNormal];
-        [button setImage:[UIImage imageNamed:@"gesture_node_highlighted"] forState:UIControlStateHighlighted];
+        button.tag = i;
+        [button setImage:[UIImage imageNamed:@"gesture_node_normal"] forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:@"gesture_node_highlighted"] forState:UIControlStateSelected];
+        // 不允许用户交互
+        button.userInteractionEnabled = NO;
         [self addSubview:button];
     }
 }
@@ -59,9 +70,117 @@
         row = i / 3;
         col = i % 3;
         btnX = margin + (margin + btnW) * col;
-        btnY = margin + (margin + btnH) * row;
+        //        btnY = margin + (margin + btnH) * row;
+        btnY = (margin + btnH) * row;
         button.frame = CGRectMake(btnX, btnY, btnW, btnH);
     }
+}
+
+-(CGPoint)pointWithTouches:(NSSet *)touches {
+    UITouch *touch = [touches anyObject];
+    return [touch locationInView:self];
+}
+
+//-(UIButton *)buttonWithPoint:(CGPoint)point {
+//    for (UIButton *btn in self.subviews) {
+//        // 要用frame，使其在同一个坐标系中，不能用bounds
+//        if(CGRectContainsPoint(btn.frame, point)) {
+//            return btn;
+//        }
+//    }
+//    return nil;
+//}
+
+-(UIButton *)buttonWithPoint:(CGPoint)point {
+    CGFloat wh = 30;
+    
+    for (UIButton *btn in self.subviews) {
+        CGFloat x = btn.center.x - wh * 0.5;
+        CGFloat y = btn.center.y - wh * 0.5;
+        CGRect frame = CGRectMake(x, y, wh, wh);
+        // 要用frame，使其在同一个坐标系中，不能用bounds
+        if(CGRectContainsPoint(frame, point)) {
+            return btn;
+        }
+    }
+    return nil;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    
+    // 当前触摸点
+    CGPoint pos = [self pointWithTouches:touches];
+    
+    // 获取触摸按钮
+    UIButton *btn = [self buttonWithPoint:pos];
+    if (btn && !btn.selected) {
+        btn.selected = YES;
+        [self.selectedBtns addObject:btn];
+        NSLog(@"在按钮上");
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    // 当前触摸点
+    self.moveP = [self pointWithTouches:touches];
+    
+    // 获取触摸按钮
+    UIButton *btn = [self buttonWithPoint:self.moveP];
+    if (btn && !btn.selected) {
+        btn.selected = YES;
+        [self.selectedBtns addObject:btn];
+        NSLog(@"在按钮上");
+    }
+    
+    // 重绘
+    [self setNeedsDisplay];
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSMutableString *result = [NSMutableString string];
+    for (UIButton *button in self.selectedBtns) {
+        [result appendFormat:@"%d", button.tag];
+    }
+    NSLog(@"result = %@", result);
+    
+    
+    // 取消所有按钮的选中状态
+    [self.selectedBtns makeObjectsPerformSelector:@selector(setSelected:) withObject:NO];
+    // 清空所有选中按钮
+    [self.selectedBtns removeAllObjects];
+    [self setNeedsDisplay];
+}
+
+-(NSMutableArray *)selectedBtns {
+    if(!_selectedBtns) {
+        _selectedBtns = [NSMutableArray array];
+    }
+    return _selectedBtns;
+}
+
+-(void)drawRect:(CGRect)rect {
+    if (!self.selectedBtns.count) {
+        return;
+    }
+    
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    // 所有选中按钮都连线
+    for (int i = 0; i < self.selectedBtns.count; i++) {
+        UIButton *btn = self.selectedBtns[i];
+        if (i == 0) {
+            [path moveToPoint:btn.center];
+        } else {
+            [path addLineToPoint:btn.center];
+        }
+    }
+    [path addLineToPoint:self.moveP];
+    
+    [[UIColor greenColor] setStroke];
+    path.lineWidth = 8;
+    path.lineJoinStyle = kCGLineCapRound;
+    path.lineCapStyle = kCGLineCapRound;
+    [path stroke];
 }
 
 @end
